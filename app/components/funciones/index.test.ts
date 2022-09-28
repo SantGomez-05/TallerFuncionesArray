5b16c78821bae313277252d9df575d7c3de22802
SantGomez-05 <santigomelo05@gmail.com>
2022-09-27 23:18:46 -0500

    commit x arrayFuntions

---
+++ app/components/funciones/index.test.ts
@@ -0,0 +1,22 @@
+/*
+import {mezclar} from './index.js';
+
+test ('voy a combinar 2 arreglos', () =>{
+    const resultado = mezclar(['h', 'a', 'c', 'd', 4],[7, 4, 17, 10]);
+    expect (resultado).toEqual(['h', 7, 'a', 4, 'c', 17, 'd', 10]);
+});
+*/
+/*
+import {division} from './index.js';
+
+
+test ('separar numeros pares e impares', () =>{
+    const resultado = division();
+  expect (resultado).toEqual({
+    numerosPares: [2, 4, 6, 8, 10],
+    numerosImpares:[1, 3, 5, 7, 9],
+  });
+});
+
+*/
+
---
+++ app/components/funciones/index.ts
@@ -0,0 +1,55 @@
+//Estrucura funtion
+/*const x = () => {
+    };
+    */
+/*   
+//1
+export const mezclar = (a: Array<number | string>, b: Array<number | string>) => {
+    const listax = [];
+    listax[0] = a[0];
+    listax[1] = b[0];
+    
+    listax[2] = a[1];
+    listax[3] = b[1];
+
+    listax[4] = a[2];
+    listax[5] = b[2];
+    
+    listax[6] = a[3];
+    listax[7] = b[3];
+
+    return listax;
+};
+*/
+//2
+export const division = () => {
+    const listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const numerosPares = [];
+    const numerosImpares = [];
+
+    for (let i = 0; i < listaNumeros.length; i++) {
+        if (i % 2 === 0) {
+            numerosPares.push(listaNumeros[i]);
+        }
+        else {
+            numerosImpares.push(listaNumeros[i]);
+        }
+    }
+    console.log('Los numeros son' + numerosPares);
+    console.log('Los numeros son' + numerosImpares);
+    return {
+        numerosPares, numerosImpares,
+    };
+};
+
+//3
+
+
+
+
+
+
+
+
+
+
